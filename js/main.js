09e63176529b0fb1cfbb5bc181cfce375cf58d9d
AdiMittel <84464564+AdiMittel@users.noreply.github.com>
2021-05-22 19:18:14 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,23 +2,30 @@
 
 var MINE = '💣'
 var EMPTY = ' '
+var MARK = '🚩'
 
 var gBoard;
 var gGame = {
-    score: 0,
     isOn: false,
+    shownCount: 0,
+    markedCount: 0,
+    secsPassed: 0
+
 }
 var gLevel = {
     size: 4,
-    mines: 2
+    mines: 3
 };
 var isShown = false
+
+
 
 function init() {
     console.log('Hello');
     gBoard = buildBoard()
     renderBoard(gBoard)
-    
+    console.log(gBoard);
+
 }
 function buildBoard() {
     var board = []
@@ -31,24 +38,131 @@ function buildBoard() {
                 isMine: false,
                 isMarked: false
             }
+
+
         }
     }
+    setRandomMines()
+    setMinesNegsCount(board)
     return board;
 }
 
 function renderBoard(board) {
     console.log(board);
+    var cellClass;
+    setRandomMines(gLevel.mines,gBoard,cellClass)
     var strHtml = '<table><tbody>';
     for (var i = 0; i < board.length; i++) {
         strHtml += '<tr>';
         for (var j = 0; j < board[0].length; j++) {
-            strHtml += `<td class="cell" onClick ="cellClicked(this)">${EMPTY}</td>`
+            cellClass = getClassName({ i: i, j: j })
+            // setRandomMines(gLevel.mines,gBoard,cellClass)
+            if (board[i][j].isMine === false) {
+                strHtml += `<td class="cell ${cellClass}" onClick ="cellClicked(this,${i},${j})">${EMPTY}</td>`;
+
+            } else {
+                strHtml += `<td class="cell ${cellClass}" onClick ="cellClicked(this,${i},${j})">${MINE}</td>`
+            }
+
         }
         strHtml += '</tr>'
     }
-    strHtml+='</tbody></table>'
+    strHtml += '</tbody></table>'
     var elTbody = document.querySelector('.board-container')
     console.log(strHtml);
     console.log(elTbody);
     elTbody.innerHTML = strHtml
+}
+
+
+
+
+function cellClicked(elCell, i, j) {
+    console.log(elCell);
+    elCell.innerText = gBoard[i][j].minesAroundCount
+
+    if (elCell === EMPTY) {
+        elCell.isShown = true;
+
+    }
+
+    // model
+    // gBoard[cellI][cellJ] = ;
+
+    // dom
+
+
+
+}
+
+function difficulty(elBtn) {
+    if (elBtn.innerText === 'Easy') {
+        gLevel.size = 4
+        gLevel.mines = 3
+    }
+    if (elBtn.innerText === 'Medium') {
+        gLevel.size = 6
+        gLevel.mines = 6
+    }
+    if (elBtn.innerText === 'Hard') {
+        gLevel.size = 8
+        gLevel.mines = 12
+    }
+    init()
+}
+
+
+
+function getClassName(location) {
+    var cellClass = 'cell-' + location.i + '-' + location.j;
+    return cellClass;
+}
+function getCellCoord(strCellClasss) {
+    var parts = strCellClasss.split('-')
+    var coord = { i: +parts[1], j: +parts[2] };
+    return coord;
+}
+function setMinesNegsCount(board) {
+
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[0].length; j++) {
+
+            board[i][j].minesAroundCount = countNegs(i, j, board)
+        }
+    }
+    // console.log(board);
+    return board;
+}
+
+function setRandomMines(minesNum,board,currCell){
+	console.log(gLevel.mines);
+    for (var i = 0; i < minesNum; i++) {
+        var randomI = getRandomIntInclusive(0, board.length - 1)
+        var randomJ = getRandomIntInclusive(0, board.length - 1)
+        if (board[randomI][randomJ] === currCell) {
+            // i--
+            continue
+        }
+        else board[randomI][randomJ].isMine = true
+    }
+    
+}
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); 
+}
+function countNegs(cellI, cellJ, board) {
+    var negsCount = 0;
+    for (var i = cellI - 1; i <= cellI + 1; i++) {
+        if (i < 0 || i >= board.length) continue;
+        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+
+            if (i === cellI && j === cellJ) continue;
+            if (j < 0 || j >= board[i].length) continue;
+            if (board[i][j].isMine === true) negsCount++;
+        }
+    }
+    console.log(negsCount);
+    return negsCount;
 }
--- js/utils.js
+++ js/utils.js
@@ -11,22 +11,22 @@ function createMat(ROWS, COLS) {
     }
     return mat;
 }
-function printMat(mat, selector) {
-    console.log(mat);
-    var strHTML = '<table border="1px black solid"><tbody>';
-    for (var i = 0; i < 4; i++) {
-        strHTML += '<tr>';
-        for (var j = 0; j < 4; j++) {
-            var cell = mat[i][j];
-            var className = 'cell cell' + i + '-' + j;
-            strHTML += '<td class="' + className + '"> ' + cell + ' </td>'
-        }
-        strHTML += '</tr>'
-    }
-    strHTML += '</tbody></table>';
-    var elContainer = document.querySelector(selector);
-    elContainer.innerHTML = strHTML;
-}
+// function printMat(mat, selector) {
+//     console.log(mat);
+//     var strHTML = '<table border="1px black solid"><tbody>';
+//     for (var i = 0; i < 4; i++) {
+//         strHTML += '<tr>';
+//         for (var j = 0; j < 4; j++) {
+//             var cell = mat[i][j];
+//             var className = 'cell cell' + i + '-' + j;
+//             strHTML += '<td class="' + className + '"> ' + cell + ' </td>'
+//         }
+//         strHTML += '</tr>'
+//     }
+//     strHTML += '</tbody></table>';
+//     var elContainer = document.querySelector(selector);
+//     elContainer.innerHTML = strHTML;
+// }
 
 function renderCell(location, value) {
     var elCell = document.querySelector(`.cell${location.i}-${location.j}`);
